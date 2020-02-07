b7d03784d0c802ffb6254ae7dd7bcc655c8660e4
Andy <aking27@wisc.edu>
2020-02-07 15:36:37 -0600

    Just added the EditText java code

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,10 +2,20 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.View;
+
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickFunction(View view){
+        Log.i("info","Button pressed");
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
